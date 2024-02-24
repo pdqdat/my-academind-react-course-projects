27cc4f70d23a2b421d21370f3c7db46586e278f5
Dat Phan <phanduongquocdat273@gmail.com>
2024-02-24 14:21:36 +0700

    complete module 3 starting project

---
+++ 03 React Essentials/01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,21 @@
+//* Default usage of props
+// export default function CoreConcept(props) {
+//     return (
+//         <li>
+//             <img src={props.image} alt={props.title} />
+//             <h3>{props.title}</h3>
+//             <p>{props.description}</p>
+//         </li>
+//     );
+// }
+
+//* Destructuring props
+export default function CoreConcept({ image, title, description }) {
+    return (
+        <li>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    );
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# my-academind-react-course-projects
+# "React - The Complete Guide 2024" Projects
+
+This repository contains the code for my projects as I learn React - The Complete Guide 2024 (incl. React Router & Redux) at Udemy, taught by Academind.
+
+### Module 3: React Essentials
